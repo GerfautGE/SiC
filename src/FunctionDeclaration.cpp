02f06ec1f78797b9e4ce47d8bfeb072e6459453d
Gerf <73294402+GerfautGE@users.noreply.github.com>
2025-01-17 19:55:55 +0100

    set a specific linkage for main function

--- src/FunctionDeclaration.cpp
+++ src/FunctionDeclaration.cpp
@@ -1,6 +1,7 @@
 #include "FunctionDeclaration.hpp"
 #include "AstNode.hpp"
 #include "Globals.hpp"
+#include "llvm/IR/Function.h"
 #include "llvm/IR/Value.h"
 #include "llvm/IR/Verifier.h"
 
@@ -8,9 +9,16 @@ FunctionDeclaration::FunctionDeclaration(Identifier *name, InstrList *body)
     : id(name), body(body) {}
 
 llvm::Value *FunctionDeclaration::codeGen() {
+  // set the linkage to external if the function is main
+  llvm::GlobalValue::LinkageTypes linkage;
+  if (id->to_string() == "main") {
+    linkage = llvm::Function::ExternalLinkage;
+  } else {
+    linkage = llvm::Function::InternalLinkage;
+  }
   TheFunction = llvm::Function::Create(
       llvm::FunctionType::get(llvm::Type::getInt32Ty(*TheContext), false),
-      llvm::Function::CommonLinkage, id->to_string(), TheModule);
+      linkage, id->to_string(), TheModule);
   auto insertBlock =
       llvm::BasicBlock::Create(*TheContext, id->to_string(), TheFunction);
   Builder->SetInsertPoint(insertBlock);
